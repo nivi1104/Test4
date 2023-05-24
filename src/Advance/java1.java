d18970658ce270f463ade198a9c203a2ef1ed30d
Nivedita Gavade <nivedita.gavade19@gmail.com>
2023-05-24 13:49:15 +0530

    Check

--- src/Advance/java1.java
+++ src/Advance/java1.java
@@ -1,5 +1,10 @@
 package Advance;
 
 public class java1 {
-
+		public static void main(String[] args) {
+			System.out.println("nnnnnnnnnnnnnnnnnnnn");
+			
+			
+			System.out.println("RRRRRRRRRRRRRRRRRRm");
+		}
 }
--- src/adv/python.java
+++ src/adv/python.java
@@ -1,5 +1,13 @@
 package adv;
 
 public class python {
+	public static void main(String[] args) {
+		
+	
+	System.out.println("11111111111111111111");
+	
+	
+	System.out.println("66666666666666666");
 
+}
 }
